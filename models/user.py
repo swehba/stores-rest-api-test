9dd0c18ae2f20ff926847a8c0d28de998a02ea7a
Steve Wehba <swehba@blastmotion.com>
2017-11-12 10:38:02 -0800

    Lesson 72

---
+++ models/user.py
@@ -0,0 +1,32 @@
+from db import db
+
+
+class UserModel(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(80))
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f'<UserModel username={self.username}>'
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.remove(self)
+        db.session.commit()
+
+    @classmethod
+    def find_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
+
+    @classmethod
+    def find_by_id(cls, _id):
+        return cls.query.filter_by(id=_id).first()
---
+++ resources/user.py
@@ -0,0 +1,34 @@
+from flask_restful import Resource, reqparse
+from models.user import UserModel
+
+
+class UserRegister(Resource):
+    """
+    This resource allows users to register by sending a POST request with their username and password.
+    """
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+                        type=str,
+                        required=True,
+                        help="The user's login identifier. Required.")
+    parser.add_argument('password',
+                        type=str,
+                        required=True,
+                        help="The user's password. Required.")
+
+    def post(self, username, password):
+        data = UserRegister.parser.parse_args()
+        username = data['username']
+        password = data['password']
+
+        if UserModel.find_by_username(username):
+            return {'message': f'A user with the username [{username}] already exists.'}, 400
+        else:
+            user = UserModel(**data)
+            user.save_to_db()
+            return {'message': f'A user with username [{username}] was successfully created.'}, 201
+
+
+class UserResource(Resource):
+    pass
